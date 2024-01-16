23d7b282e4621c2210c544bea4e1f0d030346d50
wilker <plgu@hotmail.com>
2024-01-16 12:32:27 +0800

    successful run jest test in vite

---
+++ packages/web/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  verbose: true,
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+};
--- packages/web/src/App.test.js
+++ packages/web/src/App.test.js
@@ -4,6 +4,5 @@ import App from "./App";
 it("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(true).toBe(true);
-  // expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();
 });
