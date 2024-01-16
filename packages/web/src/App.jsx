5b91f248d4450295e57310fc7f9fc544ccb51cb5
wilker <plgu@hotmail.com>
2024-01-16 12:55:15 +0800

    introuced prettier

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div>learn react</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>learn react</div>
+        </div>
+    );
 }
 
 export default App;
